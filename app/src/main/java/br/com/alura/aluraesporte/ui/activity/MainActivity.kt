86f2ad3a203ddd886e07cb943a5ca1e14400b46d
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-04-16 11:50:23 -0300

    adicionado configuracoes atualizadas para chamada do Firebase

--- app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
@@ -1,14 +1,19 @@
 package br.com.alura.aluraesporte.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import br.com.alura.aluraesporte.R
 import br.com.alura.aluraesporte.ui.viewmodel.EstadoAppViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.main_activity.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -19,9 +24,24 @@ class MainActivity : AppCompatActivity() {
     }
     private val viewModel: EstadoAppViewModel by viewModel()
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        firebaseAuth = Firebase.auth
+
+       firebaseAuth.createUserWithEmailAndPassword(
+            "pablo@aluraesporte.com", "teste123"
+        ).addOnSuccessListener { task->
+           Toast.makeText(this, "usuario foi cadastrado com sucesso", Toast.LENGTH_LONG).show()
+       }.addOnFailureListener {
+           Log.i("exception cadastro", "Erro: ${it.message}")
+           Toast.makeText(this, "deu erro $it", Toast.LENGTH_LONG).show()
+       }
+
         controlador.addOnDestinationChangedListener { _,
                                                       destination,
                                                       _ ->
